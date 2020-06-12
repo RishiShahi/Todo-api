141a1760722112e3a942e59be3b647d1daa406df
rishi shahi <rishishahi92@gmail.com>
2020-06-12 20:48:57 +0530

    Add user model and POST /users request.

--- server.js
+++ server.js
@@ -104,6 +104,16 @@ app.post('/todos', function (req, res) {
     // res.json(body);
 });
 
+app.post('/users', function (req, res) {
+  var body = _.pick(req.body, 'email', 'password');
+  
+  db.user.create(body).then(function (user) {
+      res.json(user.toJSON());
+  }, function (e) {
+      res.status(400).json(e);
+  });
+});
+
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10 );
 
